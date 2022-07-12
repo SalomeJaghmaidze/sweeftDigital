0f1f533bd56d516cd0ed71fe0eadac2a27fbe113
SalomeJaghmaidze <85635736+SalomeJaghmaidze@users.noreply.github.com>
2022-07-12 18:07:57 +0400

    main page

---
+++ src/pages/main/MainStyles.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+display: grid;
+grid-template-columns: repeat(4, 1fr);
+width: 90%;
+margin: 80px auto;
+gap: 50px;
+`
+export const Card = styled.div`
+border: 1px solid black;
+
+`
+
+export const Image = styled.img`
+width: 100%;
+`
